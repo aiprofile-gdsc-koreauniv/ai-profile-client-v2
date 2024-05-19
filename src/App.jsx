9f0135a0bca711c445149c52881fe1402456d84e
JieunKim <78676660+mellykim123@users.noreply.github.com>
2024-05-19 20:32:51 +0900

    feat: 서비스 상태 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import Splash from "./pages/Splash";
 import Home from "./pages/Home";
 import Upload from "./pages/Upload";
@@ -9,37 +9,40 @@ import TopbarLayout from "./TopbarLayout";
 import Select from "./pages/Select";
 import PersonalInformationAgreement from "./pages/PersonalInformationAgreement";
 import WIP from "./pages/WIP";
-import { useState } from "react";
+import { useEffect } from "react";
 import { RecoilRoot } from "recoil";
-
+import API from "./utils/axios";
 function App() {
-  const location = useLocation();
-  const [ready, setReady] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const res = await API.get("/ops/service");
+      if (res.data.serviceState === "preparing") {
+        navigate("/wip");
+      }
+    };
+    fetchStatus();
+  }, []);
   return (
     <>
       <Bg>
-        {ready ? (
-          <RecoilRoot>
-            <Routes>
-              <Route path="/" element={<Splash />} />
-              <Route path="/home" element={<Home />} />
-              <Route path="/" element={<TopbarLayout />}>
-                <Route path="/select" element={<Select />} />
-                <Route path="/upload" element={<Upload />} />
-                <Route path="/complete" element={<Complete />} />
-              </Route>
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-              <Route
-                path="/personal-information-agreement"
-                element={<PersonalInformationAgreement />}
-              />
-            </Routes>
-          </RecoilRoot>
-        ) : (
+        <RecoilRoot>
           <Routes>
-            <Route path="/" element={<WIP />} />
+            <Route path="/" element={<Splash />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<TopbarLayout />}>
+              <Route path="/select" element={<Select />} />
+              <Route path="/upload" element={<Upload />} />
+              <Route path="/complete" element={<Complete />} />
+            </Route>
+            <Route path="/wip" element={<WIP />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route
+              path="/personal-information-agreement"
+              element={<PersonalInformationAgreement />}
+            />
           </Routes>
-        )}
+        </RecoilRoot>
       </Bg>
     </>
   );
